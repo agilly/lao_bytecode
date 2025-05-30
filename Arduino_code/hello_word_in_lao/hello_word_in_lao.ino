68b4524caabc4d4d9551fec7c475ff3e816e4824
Diya <dt536@cam.ac.uk>
2025-05-30 23:06:50 +0100

    some comments

--- Arduino_code/hello_word_in_lao/hello_word_in_lao.ino
+++ Arduino_code/hello_word_in_lao/hello_word_in_lao.ino
@@ -2,7 +2,7 @@
 #include <Adafruit_GFX.h>  //graphics library
 #include <Adafruit_SSD1306.h> //specific driver for OLED
 #include <avr/pgmspace.h> //for storing bitmaps in flash memory
-#include "glyph_bitmaps.h"
+#include "glyph_bitmaps.h" //the bitmap file
 
 //screen dimensions
 #define SCREEN_WIDTH 128
@@ -14,20 +14,22 @@
 #define BYTES_PER_ROW (BITMAP_WIDTH + 7)/8
 #define BITMAP_HEIGHT 30
 #define BYTES_PER_BITMAP (BYTES_PER_ROW*BITMAP_HEIGHT)
-#define NUM_UNIQUE_GLYPHS 9
+#define NUM_UNIQUE_GLYPHS 9 //should be changed depending on the number of unique glyphs required
 
 //creating an instance of the display driver
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 //loading bitmap data into flash memory
 //static const uint8_t glyph_bitmaps[BYTES_PER_BITMAP*NUM_UNIQUE_GLYPHS] PROGMEM = {...};
+//above not required since we are loading a file of bitmaps
+
 //loading word data
 const uint8_t lao_word[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 const uint8_t len_word = sizeof(lao_word) / sizeof(lao_word[0]);
 
 
 uint8_t glyph_buffer[BYTES_PER_BITMAP];
-int scroll_offset = -SCREEN_WIDTH;
+int scroll_offset = -SCREEN_WIDTH; //first character is displayed at the rightmost point
 
 
 void setup() {
@@ -35,24 +37,24 @@ void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0X3C); //0X3C is the I2C address of the OLED controller
   display.clearDisplay();
   display.display();
-  delay(2000);
+  delay(2000);  //clear the display and wait
 }
 
 
 void loop() {
   display.clearDisplay();
   int x = -scroll_offset;
-  int y = (SCREEN_HEIGHT - BITMAP_HEIGHT)/2;
+  int y = (SCREEN_HEIGHT - BITMAP_HEIGHT)/2;  //y does not change
   for (int i =0; i<len_word; i++) {
     int glyph_index = lao_word[i];
-    memcpy_P(glyph_buffer, glyph_bitmaps + (glyph_index * BYTES_PER_BITMAP), BYTES_PER_BITMAP);
-    display.drawBitmap(x, y, glyph_buffer, BITMAP_WIDTH, BITMAP_HEIGHT, SSD1306_WHITE);
-    x +=BITMAP_WIDTH/2;
+    memcpy_P(glyph_buffer, glyph_bitmaps + (glyph_index * BYTES_PER_BITMAP), BYTES_PER_BITMAP); //loads the glyph buffer with a bitmap from the file
+    display.drawBitmap(x, y, glyph_buffer, BITMAP_WIDTH, BITMAP_HEIGHT, SSD1306_WHITE); //draws a grapheme
+    x +=BITMAP_WIDTH/2; //moves along by BITMAP_WIDTH to the next grapheme position
   }
-  display.display();
+  display.display(); //display the word 
   delay(5);
 
-  scroll_offset++;
+  scroll_offset++; //move scroll_offset by 1 pixel
   if (scroll_offset > BITMAP_WIDTH * len_word / 2) {
     scroll_offset = -SCREEN_WIDTH; // reset for looping scroll
   }
