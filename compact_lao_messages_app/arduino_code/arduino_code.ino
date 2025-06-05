72513880510b65f78e716ca9097ccf940285537e
Diya <dt536@cam.ac.uk>
2025-06-05 13:34:15 +0100

    arduino code

--- compact_lao_messages_app/arduino_code/arduino_code.ino
+++ compact_lao_messages_app/arduino_code/arduino_code.ino
@@ -11,27 +11,34 @@
 #define OLED_RESET -1 //not using a reset pin
 
 //bitmap details
-#define BYTES_PER_ROW (GLYPH_WIDTH + 7)/8
-#define BYTES_PER_BITMAP (BYTES_PER_ROW*GLYPH_HEIGHT)
+//#define BYTES_PER_ROW (GLYPH_WIDTH + 7)/8
+//#define BYTES_PER_BITMAP (BYTES_PER_ROW*GLYPH_HEIGHT)
 
 //creating an instance of the display driver
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
-uint8_t glyph_buffer[BYTES_PER_BITMAP];
-
 
 void scrollPhrase(const uint8_t* lao_phrase, uint8_t len_phrase){
   int scroll_offset = -SCREEN_WIDTH;   //first character to be displayed at rightmost point
-  while (scroll_offset <= GLYPH_WIDTH * len_phrase / 2){
+  int total_scroll_width = 0;
+  for (int i = 0; i < len_phrase; i++) {
+    total_scroll_width += glyph_widths[lao_phrase[i]] / 2;
+  }
+  while (scroll_offset <= total_scroll_width * len_phrase / 2){
     display.clearDisplay();
     int x = -scroll_offset;
     int y = (SCREEN_HEIGHT - GLYPH_HEIGHT)/2;  //y does not change
     for (int i =0; i<len_phrase; i++) {
       int glyph_index = lao_phrase[i];
-      memcpy_P(glyph_buffer, glyph_bitmaps + (glyph_index * BYTES_PER_BITMAP), BYTES_PER_BITMAP); //loads the glyph buffer with a bitmap from the file
-      display.drawBitmap(x, y, glyph_buffer, GLYPH_WIDTH, GLYPH_HEIGHT, SSD1306_WHITE); //draws a grapheme
-      x +=GLYPH_WIDTH/2; //moves along by GLYPH_WIDTH to the next grapheme position
+      int bitmap_start = bitmap_starts[glyph_index];
+      int glyph_width = glyph_widths[glyph_index];
+      int BYTES_PER_ROW = (glyph_width + 7)/8;
+      int BYTES_PER_BITMAP = BYTES_PER_ROW*GLYPH_HEIGHT;
+      uint8_t glyph_buffer[BYTES_PER_BITMAP];
+      memcpy_P(glyph_buffer, glyph_bitmaps + bitmap_start, BYTES_PER_BITMAP); //loads the glyph buffer with a bitmap from the file
+      display.drawBitmap(x, y, glyph_buffer, glyph_width, GLYPH_HEIGHT, SSD1306_WHITE); //draws a grapheme
+      x +=glyph_width/2; //moves along by glyph_width to the next grapheme position
     }
     display.display(); //display the word 
     delay(5);
